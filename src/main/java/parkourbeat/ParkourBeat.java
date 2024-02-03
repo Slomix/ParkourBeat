5775bf8d101c3def943b8e5d25ca6f6765365194
Slomix <soulout000@gmail.com>
2024-02-03 16:19:20 +0300

    Start

--- src/main/java/parkourbeat/ParkourBeat.java
+++ src/main/java/parkourbeat/ParkourBeat.java
@@ -2,17 +2,13 @@ package parkourbeat;
 
 import parkourbeat.commands.PlayCommand;
 import parkourbeat.commands.TpToWorldCommand;
-import parkourbeat.commands.TpToWorldTabComplete;
 import parkourbeat.data.Settings;
-import parkourbeat.levels.settings.GameSettings;
 import parkourbeat.listeners.EventListener;
 import org.bukkit.Bukkit;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 import parkourbeat.levels.dao.FileLevelSettingDAO;
 import parkourbeat.levels.LevelsManager;
-
-import java.util.Set;
 
 public class ParkourBeat extends JavaPlugin {
     private static ParkourBeat plugin;
@@ -27,10 +23,14 @@ public class ParkourBeat extends JavaPlugin {
         plugin = this;
         Settings.load();
         levelsManager = new LevelsManager(new FileLevelSettingDAO("settings"));
+
         PluginCommand tpToWorldCommand = getCommand("tptoworld");
-        tpToWorldCommand.setExecutor(new TpToWorldCommand());
-        tpToWorldCommand.setTabCompleter(new TpToWorldTabComplete());
+        TpToWorldCommand command = new TpToWorldCommand();
+        tpToWorldCommand.setExecutor(command);
+        tpToWorldCommand.setTabCompleter(command);
+
         getCommand("play").setExecutor(new PlayCommand());
+
         Bukkit.getPluginManager().registerEvents(new EventListener(), this);
     }
 
