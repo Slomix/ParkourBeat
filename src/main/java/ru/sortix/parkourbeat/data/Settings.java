d805036ee969a2ce004ba8c4fb6373d95cc6c9c1
Danila Mitrofanov <DymethInfo@gmail.com>
2024-02-22 05:36:01 +0700

    Fixed lobby world auto loading

--- src/main/java/ru/sortix/parkourbeat/data/Settings.java
+++ src/main/java/ru/sortix/parkourbeat/data/Settings.java
@@ -1,8 +1,9 @@
 package ru.sortix.parkourbeat.data;
 
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.World;
+import java.io.File;
+import javax.annotation.Nullable;
+import lombok.NonNull;
+import org.bukkit.*;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -14,22 +15,55 @@ public class Settings {
     private static boolean isLoaded = false;
 
     public static void load() {
+        JavaPlugin plugin = ParkourBeat.getPlugin();
         if (isLoaded) {
-            Bukkit.getLogger().warning("Settings already loaded!");
+            plugin.getLogger().warning("Settings already loaded!");
             return;
         }
-        JavaPlugin plugin = ParkourBeat.getPlugin();
         plugin.saveDefaultConfig();
         FileConfiguration config = plugin.getConfig();
         ConfigurationSection spawnSection = config.getConfigurationSection("lobby");
         lobbySpawn = getLocation(spawnSection);
-        World spawnWorld = Bukkit.getWorld(spawnSection.getString("world"));
-        if (spawnWorld == null) {
-            throw new IllegalStateException("No world for spawn with provided name is present!");
+
+        World spawnWorld;
+        try {
+            spawnWorld = getOrLoadWord(plugin.getServer(), spawnSection.getString("world"));
+        } catch (Exception e) {
+            throw new RuntimeException("Unable to load lobby world", e);
         }
+
         lobbySpawn.setWorld(spawnWorld);
         defaultWorldSpawn = getLocation(config.getConfigurationSection("default_world"));
         isLoaded = true;
+    }
+
+    @NonNull private static World getOrLoadWord(@NonNull Server server, @Nullable String worldName) {
+        if (worldName == null) {
+            throw new IllegalArgumentException("World name not found");
+        }
+
+        World result = server.getWorld(worldName);
+        if (result != null) return result;
+
+        File worldDir = new File(server.getWorldContainer(), worldName);
+        if (!worldDir.isDirectory()) {
+            throw new IllegalArgumentException("Is not a directory: " + worldDir.getAbsolutePath());
+        }
+
+        WorldCreator worldCreator = new WorldCreator(worldName);
+        worldCreator.seed(0L);
+        worldCreator.environment(World.Environment.NORMAL);
+        worldCreator.type(WorldType.FLAT);
+        worldCreator.generateStructures(false);
+
+        result = server.createWorld(worldCreator);
+        if (result != null) return result;
+
+        throw new UnsupportedOperationException(
+                "Unable to create world \""
+                        + worldName
+                        + "\" from directory "
+                        + worldDir.getAbsolutePath());
     }
 
     private static Location getLocation(ConfigurationSection config) {
