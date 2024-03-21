62a05bdb634060d7e9a270ebcb70bf5f6df0224e
Роман Аникушин <r.anikushin@cft.ru>
2024-03-21 14:24:22 +0700

    Added @Deprecation annotation for deprecated commands

--- src/main/java/ru/sortix/parkourbeat/commands/CommandColor.java
+++ src/main/java/ru/sortix/parkourbeat/commands/CommandColor.java
@@ -7,7 +7,11 @@ import ru.sortix.parkourbeat.constant.Messages;
 
 import static ru.sortix.parkourbeat.constant.PermissionConstants.COMMAND_PERMISSION;
 
+/**
+ * @deprecated the player must now use a clickable item instead of a command
+ */
 @Command(name = "color")
+@Deprecated(since = "1.0-SNAPSHOT", forRemoval = true)
 public class CommandColor {
 
     @Execute
