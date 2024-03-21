e9c42610973ed10a0b3c6b882a8b881b976e24e2
Danila Mitrofanov <DymethInfo@gmail.com>
2024-03-22 01:42:55 +0700

    EditorMainMenu refactoring

--- src/main/java/ru/sortix/parkourbeat/inventory/type/editor/EditorMainMenu.java
+++ src/main/java/ru/sortix/parkourbeat/inventory/type/editor/EditorMainMenu.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 import java.util.List;
 import lombok.NonNull;
 import net.md_5.bungee.api.ChatColor;
-import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Color;
 import org.bukkit.Location;
@@ -18,6 +17,7 @@ import ru.sortix.parkourbeat.activity.type.EditActivity;
 import ru.sortix.parkourbeat.commands.CommandDelete;
 import ru.sortix.parkourbeat.data.Settings;
 import ru.sortix.parkourbeat.inventory.ParkourBeatInventory;
+import ru.sortix.parkourbeat.inventory.event.ClickEvent;
 import ru.sortix.parkourbeat.item.ItemUtils;
 import ru.sortix.parkourbeat.item.editor.type.EditTrackPointsItem;
 import ru.sortix.parkourbeat.levels.settings.LevelSettings;
@@ -26,9 +26,12 @@ import ru.sortix.parkourbeat.player.input.PlayersInputManager;
 import ru.sortix.parkourbeat.utils.TeleportUtils;
 
 public class EditorMainMenu extends ParkourBeatInventory {
+    private final EditActivity activity;
+
     @SuppressWarnings("deprecation")
     public EditorMainMenu(@NonNull ParkourBeat plugin, @NonNull EditActivity activity) {
         super(plugin, 5, "Параметры уровня");
+        this.activity = activity;
         this.setItem(
                 1,
                 5,
@@ -36,14 +39,7 @@ public class EditorMainMenu extends ParkourBeatInventory {
                     meta.setDisplayName(ChatColor.GOLD + "Покинуть редактор");
                     meta.setLore(List.of(ChatColor.YELLOW + "Блоки и настройки будут сохранены"));
                 }),
-                event -> {
-                    Player player = event.getPlayer();
-                    TeleportUtils.teleportAsync(this.plugin, player, Settings.getLobbySpawn())
-                            .thenAccept(success -> {
-                                if (!success) return;
-                                this.plugin.get(ActivityManager.class).setActivity(player, null);
-                            });
-                });
+                this::leaveEditor);
         this.setItem(
                 3,
                 2,
@@ -55,49 +51,7 @@ public class EditorMainMenu extends ParkourBeatInventory {
                             ChatColor.YELLOW + "Вам потребуется указать в чате",
                             ChatColor.YELLOW + "HEX-цвет. Например: #FFCC66"));
                 }),
-                event -> {
-                    Player player = event.getPlayer();
-                    player.closeInventory();
-
-                    PlayersInputManager manager = this.plugin.get(PlayersInputManager.class);
-                    if (manager.isInputRequested(player)) {
-                        player.sendMessage("Функция недоступна в данный момент");
-                        return;
-                    }
-
-                    player.sendMessage("У вас есть 30 сек, чтобы указать в чате HEX-цвет. Например: #FFCC66");
-                    TextComponent msg1 = new TextComponent(
-                            "Подобрать цвет можно " + ChatColor.UNDERLINE + "тут" + ChatColor.RESET + " (кликабельно)");
-                    msg1.setClickEvent(
-                            new ClickEvent(ClickEvent.Action.OPEN_URL, "https://google.com/search?q=hex+палитра"));
-                    player.sendMessage(msg1);
-
-                    manager.requestChatInput(player, 20 * 30).thenAccept(message -> {
-                        if (message == null) {
-                            player.sendMessage("Цвет не выбран");
-                            return;
-                        }
-
-                        String hex = message.startsWith("#") ? message.substring(1) : message;
-                        Color color;
-                        try {
-                            int r = Integer.valueOf(hex.substring(0, 2), 16);
-                            int g = Integer.valueOf(hex.substring(2, 4), 16);
-                            int b = Integer.valueOf(hex.substring(4, 6), 16);
-                            color = Color.fromRGB(r, g, b);
-                        } catch (Exception e) {
-                            player.sendMessage("Ошибка. Пожалуйста, убедитесь, что вы ввели правильный HEX-код");
-                            return;
-                        }
-                        activity.setCurrentColor(color);
-
-                        player.sendMessage("Выбранный цвет:");
-
-                        TextComponent msg2 = new TextComponent("#" + hex);
-                        msg2.setColor(ChatColor.of("#" + hex));
-                        player.sendMessage(msg2);
-                    });
-                });
+                this::selectParticlesColor);
         this.setItem(
                 3,
                 4,
@@ -113,11 +67,7 @@ public class EditorMainMenu extends ParkourBeatInventory {
                             ChatColor.YELLOW + "Текущая композиция:",
                             ChatColor.YELLOW + (song == null ? "отсутствует" : song.getSongName())));
                 }),
-                event -> {
-                    Player player = event.getPlayer();
-
-                    new SelectSongMenu(plugin, activity.getLevel()).open(player);
-                });
+                this::selectLevelSong);
         this.setItem(
                 3,
                 6,
@@ -128,24 +78,7 @@ public class EditorMainMenu extends ParkourBeatInventory {
                             ChatColor.YELLOW + "Направление взгляда игроков будет точно таким же,",
                             ChatColor.YELLOW + "как при установке точки спауна"));
                 }),
-                event -> {
-                    Player player = event.getPlayer();
-                    player.closeInventory();
-
-                    LevelSettings levelSettings = activity.getLevel().getLevelSettings();
-                    Location playerLocation = player.getLocation();
-
-                    if (!isValidSpawnPoint(playerLocation, levelSettings)) {
-                        player.sendMessage("Точка спауна не может быть установлена здесь.");
-                        return;
-                    }
-
-                    levelSettings.getWorldSettings().setSpawn(playerLocation);
-
-                    player.sendMessage("Точка спауна установлена на уровне ваших ног. "
-                            + "Убедитесь, что направление взгляда выбрано корректно! "
-                            + "Именно в эту сторону будут повёрнуты игроки при телепортации");
-                });
+                this::setSpawnPoint);
         this.setItem(
                 3,
                 8,
@@ -155,45 +88,7 @@ public class EditorMainMenu extends ParkourBeatInventory {
                             ChatColor.YELLOW + "Вам будет необходимо отправить",
                             ChatColor.YELLOW + "новое название в чат"));
                 }),
-                event -> {
-                    Player player = event.getPlayer();
-                    player.closeInventory();
-
-                    PlayersInputManager manager = this.plugin.get(PlayersInputManager.class);
-                    if (manager.isInputRequested(player)) {
-                        player.sendMessage("Функция недоступна в данный момент");
-                        return;
-                    }
-
-                    player.sendMessage("У вас есть 30 сек, чтобы указать в чате новое название уровня");
-                    manager.requestChatInput(player, 20 * 30).thenAccept(newName -> {
-                        if (newName == null) {
-                            player.sendMessage("Новое название не введено");
-                            return;
-                        }
-
-                        newName = ChatColor.translateAlternateColorCodes('&', newName);
-                        String plainName = ChatColor.stripColor(newName);
-
-                        if (plainName.length() < 3) {
-                            player.sendMessage("Название должно содержать от 3 до 30 символов");
-                            return;
-                        }
-
-                        if (plainName.length() > 30) {
-                            player.sendMessage("Название должно содержать от 5 до 30 символов");
-                            return;
-                        }
-
-                        String oldName;
-
-                        oldName = activity.getLevel().getDisplayName();
-                        activity.getLevel().getLevelSettings().getGameSettings().setDisplayName(newName);
-                        newName = activity.getLevel().getDisplayName();
-
-                        player.sendMessage("Название изменено с \"" + oldName + "\" на \"" + newName + "\"");
-                    });
-                });
+                this::renameLevel);
         this.setItem(
                 5,
                 3,
@@ -203,13 +98,7 @@ public class EditorMainMenu extends ParkourBeatInventory {
                             ChatColor.RED + "" + ChatColor.BOLD + "Все установленные точки трека",
                             ChatColor.RED + "" + ChatColor.BOLD + "будут БЕЗВОЗВРАТНО удалены"));
                 }),
-                event -> {
-                    Player player = event.getPlayer();
-                    player.closeInventory();
-
-                    EditTrackPointsItem.clearAllPoints(activity.getLevel());
-                    player.sendMessage("Все точки сброшены");
-                });
+                this::resetAllTrackPoints);
         this.setItem(
                 5,
                 7,
@@ -219,14 +108,143 @@ public class EditorMainMenu extends ParkourBeatInventory {
                             ChatColor.RED + "" + ChatColor.BOLD + "Уровень будет удалён",
                             ChatColor.RED + "" + ChatColor.BOLD + "БЕЗ возможности восстановления"));
                 }),
-                event -> {
-                    Player player = event.getPlayer();
-                    player.closeInventory();
+                this::deleteLevel);
+    }
 
-                    CommandDelete.deleteLevel(
-                            plugin,
-                            player,
-                            activity.getLevel().getLevelSettings().getGameSettings());
+    private void leaveEditor(@NonNull ClickEvent event) {
+        Player player = event.getPlayer();
+
+        TeleportUtils.teleportAsync(this.plugin, player, Settings.getLobbySpawn())
+                .thenAccept(success -> {
+                    if (!success) return;
+                    this.plugin.get(ActivityManager.class).setActivity(player, null);
                 });
+    }
+
+    private void selectParticlesColor(@NonNull ClickEvent event) {
+        Player player = event.getPlayer();
+        player.closeInventory();
+
+        PlayersInputManager manager = this.plugin.get(PlayersInputManager.class);
+        if (manager.isInputRequested(player)) {
+            player.sendMessage("Функция недоступна в данный момент");
+            return;
+        }
+
+        player.sendMessage("У вас есть 30 сек, чтобы указать в чате HEX-цвет. Например: #FFCC66");
+        TextComponent msg1 = new TextComponent(
+                "Подобрать цвет можно " + ChatColor.UNDERLINE + "тут" + ChatColor.RESET + " (кликабельно)");
+        msg1.setClickEvent(new net.md_5.bungee.api.chat.ClickEvent(
+                net.md_5.bungee.api.chat.ClickEvent.Action.OPEN_URL, "https://google.com/search?q=hex+палитра"));
+        //noinspection deprecation
+        player.sendMessage(msg1);
+
+        manager.requestChatInput(player, 20 * 30).thenAccept(message -> {
+            if (message == null) {
+                player.sendMessage("Цвет не выбран");
+                return;
+            }
+
+            String hex = message.startsWith("#") ? message.substring(1) : message;
+            Color color;
+            try {
+                int r = Integer.valueOf(hex.substring(0, 2), 16);
+                int g = Integer.valueOf(hex.substring(2, 4), 16);
+                int b = Integer.valueOf(hex.substring(4, 6), 16);
+                color = Color.fromRGB(r, g, b);
+            } catch (Exception e) {
+                player.sendMessage("Ошибка. Пожалуйста, убедитесь, что вы ввели правильный HEX-код");
+                return;
+            }
+            this.activity.setCurrentColor(color);
+
+            player.sendMessage("Выбранный цвет:");
+
+            TextComponent msg2 = new TextComponent("#" + hex);
+            msg2.setColor(ChatColor.of("#" + hex));
+            //noinspection deprecation
+            player.sendMessage(msg2);
+        });
+    }
+
+    private void selectLevelSong(@NonNull ClickEvent event) {
+        Player player = event.getPlayer();
+
+        new SelectSongMenu(this.plugin, this.activity.getLevel()).open(player);
+    }
+
+    private void setSpawnPoint(@NonNull ClickEvent event) {
+        Player player = event.getPlayer();
+        player.closeInventory();
+
+        LevelSettings levelSettings = this.activity.getLevel().getLevelSettings();
+        Location playerLocation = player.getLocation();
+
+        if (!isValidSpawnPoint(playerLocation, levelSettings)) {
+            player.sendMessage("Точка спауна не может быть установлена здесь.");
+            return;
+        }
+
+        levelSettings.getWorldSettings().setSpawn(playerLocation);
+
+        player.sendMessage("Точка спауна установлена на уровне ваших ног. "
+                + "Убедитесь, что направление взгляда выбрано корректно! "
+                + "Именно в эту сторону будут повёрнуты игроки при телепортации");
+    }
+
+    private void renameLevel(@NonNull ClickEvent event) {
+        Player player = event.getPlayer();
+        player.closeInventory();
+
+        PlayersInputManager manager = this.plugin.get(PlayersInputManager.class);
+        if (manager.isInputRequested(player)) {
+            player.sendMessage("Функция недоступна в данный момент");
+            return;
+        }
+
+        player.sendMessage("У вас есть 30 сек, чтобы указать в чате новое название уровня");
+        manager.requestChatInput(player, 20 * 30).thenAccept(newName -> {
+            if (newName == null) {
+                player.sendMessage("Новое название не введено");
+                return;
+            }
+
+            newName = ChatColor.translateAlternateColorCodes('&', newName);
+            String plainName = ChatColor.stripColor(newName);
+
+            if (plainName.length() < 3) {
+                player.sendMessage("Название должно содержать от 3 до 30 символов");
+                return;
+            }
+
+            if (plainName.length() > 30) {
+                player.sendMessage("Название должно содержать от 5 до 30 символов");
+                return;
+            }
+
+            String oldName;
+
+            oldName = this.activity.getLevel().getDisplayName();
+            this.activity.getLevel().getLevelSettings().getGameSettings().setDisplayName(newName);
+            newName = this.activity.getLevel().getDisplayName();
+
+            player.sendMessage("Название изменено с \"" + oldName + "\" на \"" + newName + "\"");
+        });
+    }
+
+    private void resetAllTrackPoints(@NonNull ClickEvent event) {
+        Player player = event.getPlayer();
+        player.closeInventory();
+
+        EditTrackPointsItem.clearAllPoints(this.activity.getLevel());
+        player.sendMessage("Все точки сброшены");
+    }
+
+    private void deleteLevel(@NonNull ClickEvent event) {
+        Player player = event.getPlayer();
+        player.closeInventory();
+
+        CommandDelete.deleteLevel(
+                this.plugin, player, this.activity.getLevel().getLevelSettings().getGameSettings());
     }
 }
